7409fce73cad100d73fb86bc1b8d394b45512be7
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-07-19 14:07:04 +0530

    Global API updated with heroku

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-export const API = "http://localhost:4000";
+// export const API = "http://localhost:4000";
 
-// export const API = "https://dashboard.heroku.com/apps/hackathon2-backend-raghu";
+export const API = "https://hackathon2-backend-raghu.herokuapp.com/";
